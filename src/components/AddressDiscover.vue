6ae4a9d324f41ad480bc80c57839e652ac6bb831
Ricardo Gosch <rgosch@reweb.com.br>
2018-01-22 16:56:04 -0200

    Add filter for states

--- src/components/AddressDiscover.vue
+++ src/components/AddressDiscover.vue
@@ -13,9 +13,9 @@
                     <h2 class="form-label">Selecione o Estado:</h2>
                     <button class="form-item form-item-button" title="Selecione um Estado" @click.prevent="toggleStates = !toggleStates">{{ state ? state.name : 'Selecione' }}</button>
                     <div class="group-list" v-bind:class="{show: toggleStates}">
-                        <input type="text" class="group-filter" placeholder="Filtre pelos estados" id="filter">
+                        <input type="text" class="group-filter" placeholder="Filtre pelos estados" id="filter" v-model="filter" @input="filter = $event.target.value">
                         <div class="group-items">
-                            <div class="group-item" v-for="s in states" v-bind:key="s.uf">
+                            <div class="group-item" v-for="s in filterStates()" v-bind:key="s.uf">
                                 <input type="radio" class="form-hidden .radio" v-bind:id="s.uf" v-bind:value="s" v-bind:key="s.uf" v-model="state">
                                 <label class="group-label" v-bind:for="s.uf">{{ s.name }}</label>
                             </div>
@@ -42,104 +42,119 @@
 </template>
 
 <script>
-import axios from 'axios';
+import axios from "axios";
 export default {
-	name: 'AddressDiscover',
-	data () {
-		return {
-            cep: undefined,
-            state: undefined,
-            city: undefined,
-            address: undefined,
-            toggleStates: false,
-            states: [
-                {uf: 'AC', name: 'Acre'},
-                {uf: 'AL', name: 'Alagoas'},
-                {uf: 'AP', name: 'Amapá'},
-                {uf: 'AM', name: 'Amazonas'},
-                {uf: 'BA', name: 'Bahia'},
-                {uf: 'CE', name: 'Ceará'},
-                {uf: 'DF', name: 'Distrito Federal'},
-                {uf: 'ES', name: 'Espírito Santo'},
-                {uf: 'GO', name: 'Goiás'},
-                {uf: 'MA', name: 'Maranhão'},
-                {uf: 'MT', name: 'Mato Grosso'},
-                {uf: 'MS', name: 'Mato Grosso do Sul'},
-                {uf: 'MG', name: 'Minas Gerais'},
-                {uf: 'PA', name: 'Pará'},
-                {uf: 'PB', name: 'Paraíba'},
-                {uf: 'PR', name: 'Paraná'},
-                {uf: 'PE', name: 'Pernambuco'},
-                {uf: 'PI', name: 'Piauí'},
-                {uf: 'RJ', name: 'Rio de Janeiro'},
-                {uf: 'RN', name: 'Rio Grande do Norte'},
-                {uf: 'RS', name: 'Rio Grande do Sul'},
-                {uf: 'RO', name: 'Rondônia'},
-                {uf: 'RR', name: 'Roraima'},
-                {uf: 'SC', name: 'Santa Catarina'},
-                {uf: 'SP', name: 'São Paulo'},
-                {uf: 'SE', name: 'Sergipe'},
-                {uf: 'TO', name: 'Tocantins'},
-            ]
-        }
+  name: "AddressDiscover",
+  data() {
+    return {
+      cep: undefined,
+      state: undefined,
+      city: undefined,
+      address: undefined,
+      toggleStates: false,
+      filter: undefined,
+      states: [
+        { uf: "AC", name: "Acre" },
+        { uf: "AL", name: "Alagoas" },
+        { uf: "AP", name: "Amapá" },
+        { uf: "AM", name: "Amazonas" },
+        { uf: "BA", name: "Bahia" },
+        { uf: "CE", name: "Ceará" },
+        { uf: "DF", name: "Distrito Federal" },
+        { uf: "ES", name: "Espírito Santo" },
+        { uf: "GO", name: "Goiás" },
+        { uf: "MA", name: "Maranhão" },
+        { uf: "MT", name: "Mato Grosso" },
+        { uf: "MS", name: "Mato Grosso do Sul" },
+        { uf: "MG", name: "Minas Gerais" },
+        { uf: "PA", name: "Pará" },
+        { uf: "PB", name: "Paraíba" },
+        { uf: "PR", name: "Paraná" },
+        { uf: "PE", name: "Pernambuco" },
+        { uf: "PI", name: "Piauí" },
+        { uf: "RJ", name: "Rio de Janeiro" },
+        { uf: "RN", name: "Rio Grande do Norte" },
+        { uf: "RS", name: "Rio Grande do Sul" },
+        { uf: "RO", name: "Rondônia" },
+        { uf: "RR", name: "Roraima" },
+        { uf: "SC", name: "Santa Catarina" },
+        { uf: "SP", name: "São Paulo" },
+        { uf: "SE", name: "Sergipe" },
+        { uf: "TO", name: "Tocantins" }
+      ]
+    };
+  },
+  methods: {
+    searchByCep: function() {
+      let api = "http://viacep.com.br/ws/" + this.cep + "/json/";
+      let self = this;
+      this.api(api, function(data) {
+        if (data.error) return;
+        self.fill(data);
+      });
     },
-    methods: {
-        searchByCep: function(){
-            let api = 'http://viacep.com.br/ws/'+this.cep+'/json/';
-            let self = this;
-            this.api(api, function(data){
-                if(data.error) return;
-                self.fill(data);
-            });
-        },
-        searchByAddress: function(){
-            let api = 'http://viacep.com.br/ws/'+this.state.uf+'/'+this.city+'/'+this.address+'/json/';
-            let self = this;
-            this.api(api, function(data){
-                if(data.error) return;
-                self.fill(data[0]);
-            });
-        },
-        fill: function(data){
-            let self = this;
-            this.getState(data.uf, function(state){
-                self.state = state;
-            });
-            this.stat = data.uf;
-            this.city = data.localidade;
-            this.address = data.logradouro;
-            this.cep = data.cep;
-        },
-        api: function(url, callback) {
-            axios.get(url)
-            .then(function (response) {
-                callback(response.data);
-            })
-            .catch(e => {
-                console.error(e);
-            });
-        },
-        search: function(){
-            if(this.cep && (this.cep.length === 8 || this.cep.length === 9))
-                // Search by cep
-                this.searchByCep();
-            else
-                // Search by address
-                this.searchByAddress();
-        },
-        reset: function(){
-            this.cep = undefined;
-            this.state = undefined;
-            this.city = undefined;
-            this.address = undefined;
-        },
-        getState: function(state, callback){
-            this.states.forEach(s => {
-                if(s.uf === state) callback(s);
-            });
-        }
+    searchByAddress: function() {
+      let api =
+        "http://viacep.com.br/ws/" +
+        this.state.uf +
+        "/" +
+        this.city +
+        "/" +
+        this.address +
+        "/json/";
+      let self = this;
+      this.api(api, function(data) {
+        if (data.error) return;
+        self.fill(data[0]);
+      });
     },
-}
+    fill: function(data) {
+      let self = this;
+      this.getState(data.uf, function(state) {
+        self.state = state;
+      });
+      this.stat = data.uf;
+      this.city = data.localidade;
+      this.address = data.logradouro;
+      this.cep = data.cep;
+    },
+    api: function(url, callback) {
+      axios
+        .get(url)
+        .then(function(response) {
+          callback(response.data);
+        })
+        .catch(e => {
+          console.error(e);
+        });
+    },
+    search: function() {
+      if (this.cep && (this.cep.length === 8 || this.cep.length === 9))
+        // Search by cep
+        this.searchByCep();
+      else
+        // Search by address
+        this.searchByAddress();
+    },
+    reset: function() {
+      this.cep = undefined;
+      this.state = undefined;
+      this.city = undefined;
+      this.address = undefined;
+    },
+    getState: function(state, callback) {
+      this.states.forEach(s => {
+        if (s.uf === state) callback(s);
+      });
+    },
+    filterStates: function() {
+      if (!this.filter) return this.states;
+
+      let expression = RegExp(this.filter.trim(), "i");
+      return this.states.filter(state => expression.test(state.name));
+    }
+  }
+};
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
@@ -169,12 +184,12 @@ export default {
     height: 50px;
     border-radius: 3px;
     font-size: 14px;
-    color: #403F4C;
+    color: #403f4c;
     border: none;
     padding: 0 15px;
     text-align: left;
-    background-color: #EFE9F4;
-    font-family: 'Lato', sans-serif;
+    background-color: #efe9f4;
+    font-family: "Lato", sans-serif;
     box-shadow: none;
 }
 .group-list {
@@ -188,7 +203,7 @@ export default {
     box-shadow: 2px 2px 2px rgba(0, 0, 0, 0.25);
     pointer-events: none;
     opacity: 0;
-    transition: ease .5s;
+    transition: ease 0.5s;
 }
 .group-list.show {
     opacity: 1;
@@ -199,14 +214,14 @@ export default {
     width: calc(100% - 30px);
     height: 40px;
     padding: 0 15px;
-    color: #403F4C;
+    color: #403f4c;
     border-radius: 3px;
     border: none;
-    background-color: #EFE9F4;
+    background-color: #efe9f4;
 }
 .group-items {
     max-height: 40vh;
-    transition: ease .5s;
+    transition: ease 0.5s;
     overflow-y: auto;
     height: 0;
 }
@@ -215,10 +230,9 @@ export default {
 }
 
 .group-item {
-
 }
 .group-item .group-label {
-    color: #403F4C;
+    color: #403f4c;
     min-height: 30px;
     line-height: 30px;
     padding: 0 30px;
@@ -229,7 +243,7 @@ export default {
     font-weight: bold;
 }
 .group-item input[type="radio"]:checked + .group-label {
-    background-color: #EFE9F4;
+    background-color: #efe9f4;
 }
 .form-hidden {
     position: absolute;
@@ -246,7 +260,7 @@ export default {
     cursor: pointer;
 }
 .btn.btn-success {
-    background-color: #77CBB9;
+    background-color: #77cbb9;
     font-weight: bold;
 }
 .form-big-divisor {
@@ -256,19 +270,19 @@ export default {
     align-items: center;
     margin: 23px 0 46px 0;
 }
-.form-big-divisor::after, .form-big-divisor::before {
-    content: '';
+.form-big-divisor::after,
+.form-big-divisor::before {
+    content: "";
     width: 50px;
     height: 1px;
-    background-color: #2F2E38;
+    background-color: #2f2e38;
 }
 .form-big-divisor .text {
     text-transform: uppercase;
     font-size: 35px;
-    color: #2F2E38;
+    color: #2f2e38;
     font-weight: bold;
     margin: 0 10px;
     cursor: default;
 }
-
 </style>
